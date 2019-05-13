5514c6aeeede1c27feab98b241888eca76b35b7d
jacob <stilwell@iastate.edu>
2019-05-12 19:39:10 -0500

    basic contact

--- Crud_contacts/src/defaultClasses/Contact.java
+++ Crud_contacts/src/defaultClasses/Contact.java
@@ -2,4 +2,21 @@ package defaultClasses;
 
 public class Contact {
 
+	String name;
+	String phone;
+	String address;
+	
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public Contact(String name) {
+		this.name = name;
+	}
+	public Contact(String name, String phone) {
+		this.name = name;
+		this.phone = phone;
+	}
 }
